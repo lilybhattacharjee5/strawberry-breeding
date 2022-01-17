57912b2ec0129a20373efd2ecd797fe742e12a2a
Lily Bhattacharjee <lbhattacharjee@berkeley.edu>
2022-01-16 17:19:48 -0800

    Add functions to support generating masked files (without grouping by linkage group) for new genotype data

--- beagle_generate_masked_files_updated_data.R
+++ beagle_generate_masked_files_updated_data.R
@@ -20,6 +20,9 @@ beagle_outputs_path = glue(root_path, "new_outputs")
 dir.create(beagle_inputs_path)
 dir.create(beagle_outputs_path)
 
+dir.create(glue("{beagle_inputs_path}/masking"))
+dir.create(glue("{beagle_outputs_path}/masking"))
+
 snp_array_name = "Factorial_50K_SNP_Array_Genotypes.csv"
 name_mapper_name = "organism_safe_name_mapper_updated_data.csv"
 cam_map_name = "camMap.csv"
@@ -75,158 +78,69 @@ grouped_joined_snp_cam = filtered_joined_snp_cam %>% group_by(Chromosome) %>% ar
 filtered_joined_snp_cam = grouped_joined_snp_cam
 View(filtered_joined_snp_cam)
 
-# create genotypes VCF file
-chrom_linkage_groups <- filtered_joined_snp_cam$Chromosome
-pos_vals <- filtered_joined_snp_cam$Position
-id_vals <- rep(NA, length(pos_vals))
-ref_vals <- filtered_joined_snp_cam$REF
-alt_vals <- filtered_joined_snp_cam$ALT
-qual_vals <- rep(99, length(pos_vals))
-filter_vals <- rep(NA, length(pos_vals))
-info_vals <- rep(NA, length(pos_vals))
-format_vals <- rep("GT", length(pos_vals))
+generateMaskedFiles <- function(runNum, numMasked) {
+  dir.create(glue("{beagle_inputs_path}/masking/{runNum}_inputs/"))
+  dir.create(glue("{beagle_outputs_path}/masking/{runNum}_outputs/"))
+  filtered_joined_snp_cam_copy <- cbind(filtered_joined_snp_cam)
+  
+  # create genotypes VCF file
+  chrom_linkage_groups <- filtered_joined_snp_cam_copy$Chromosome
+  pos_vals <- filtered_joined_snp_cam_copy$Position
+  id_vals <- rep(NA, length(pos_vals))
+  ref_vals <- filtered_joined_snp_cam_copy$REF
+  alt_vals <- filtered_joined_snp_cam_copy$ALT
+  qual_vals <- rep(99, length(pos_vals))
+  filter_vals <- rep(NA, length(pos_vals))
+  info_vals <- rep(NA, length(pos_vals))
+  format_vals <- rep("GT", length(pos_vals))
+  
+  fixedData <- data.table(
+    CHROM = chrom_linkage_groups,
+    POS = pos_vals,
+    ID = id_vals,
+    REF = ref_vals,
+    ALT = alt_vals,
+    QUAL = qual_vals,
+    FILTER = filter_vals,
+    INFO = info_vals
+  )
+  
+  # replace cells: 0 => 0/0, 1 => 0/1, 2 => 1/1
+  filtered_joined_snp_cam_copy[] <- lapply(filtered_joined_snp_cam_copy, gsub, pattern = " ", replacement = "")
+  filtered_joined_snp_cam_copy[filtered_joined_snp_cam_copy == "0"] <- "0/0"
+  filtered_joined_snp_cam_copy[filtered_joined_snp_cam_copy == "1"] <- "0/1"
+  filtered_joined_snp_cam_copy[filtered_joined_snp_cam_copy == "2"] <- "1/1"
+  
+  filtered_joined_snp_cam_copy_colnames <- colnames(filtered_joined_snp_cam_copy)
+  filtered_genotype_data <- filtered_joined_snp_cam_copy[, filtered_joined_snp_cam_copy_colnames[filtered_joined_snp_cam_copy_colnames %in% name_mapper$safe_names]]
+  filtered_genotype_data$FORMAT <- rep("GT", length(pos_vals))
+  filtered_genotype_data <- filtered_genotype_data[, c(length(colnames(filtered_genotype_data)), 1:length(colnames(filtered_genotype_data)) - 1)]
 
-fixedData <- data.table(
-  CHROM = chrom_linkage_groups,
-  POS = pos_vals,
-  ID = id_vals,
-  REF = ref_vals,
-  ALT = alt_vals,
-  QUAL = qual_vals,
-  FILTER = filter_vals,
-  INFO = info_vals
-)
+  # masking
+  if (numMasked > 0) {
+    hetLocs <- which(filtered_genotype_data == "0/1", arr.ind = TRUE)
+    # randomly sample n hetLocs
+    hetLocsSelected <- hetLocs[sample(nrow(hetLocs), numMasked), ]
+    filtered_genotype_data_copy <- cbind(filtered_genotype_data)
+    
+    for (h in seq(1, nrow(hetLocsSelected))) {
+      currHetLoc <- hetLocsSelected[h, ]
+      filtered_genotype_data_copy[currHetLoc["row"], currHetLoc["col"]] = NA
+    }
+    # write masked hetLocs to file
+    write.csv(hetLocsSelected, glue("{beagle_inputs_path}/masking/{runNum}_inputs/masked_locations.csv"))
+  }
 
-# replace cells: 0 => 0/0, 1 => 0/1, 2 => 1/1
-filtered_joined_snp_cam[] <- lapply(filtered_joined_snp_cam, gsub, pattern = " ", replacement = "")
-filtered_joined_snp_cam[filtered_joined_snp_cam == "0"] <- "0/0"
-filtered_joined_snp_cam[filtered_joined_snp_cam == "1"] <- "0/1"
-filtered_joined_snp_cam[filtered_joined_snp_cam == "2"] <- "1/1"
-View(filtered_joined_snp_cam)
+  fixedData <- as.matrix(fixedData, row.names = NULL)
+  genotypeData <- as.matrix(filtered_genotype_data, row.names = NULL)
+  vcfData <- new("vcfR", meta = metaData, fix = fixedData, gt = genotypeData)
+  write.vcf(vcfData, file = glue("{beagle_inputs_path}/masking/{runNum}_inputs/genotype.vcf.gz"))
+}
 
-filtered_joined_snp_cam_colnames <- colnames(filtered_joined_snp_cam)
-filtered_genotype_data <- filtered_joined_snp_cam[, filtered_joined_snp_cam_colnames[filtered_joined_snp_cam_colnames %in% name_mapper$safe_names]]
-filtered_genotype_data$FORMAT <- rep("GT", length(pos_vals))
-filtered_genotype_data <- filtered_genotype_data[, c(length(colnames(filtered_genotype_data)), 1:length(colnames(filtered_genotype_data)) - 1)]
-
-genotypeData <- as.matrix(filtered_genotype_data, row.names = NULL)
-fixedData <- as.matrix(fixedData, row.names = NULL)
-
-View(genotypeData)
-View(fixedData)
-
-nrow(fixedData)
-
-vcfData <- new("vcfR", meta = metaData, fix = fixedData, gt = genotypeData)
-write.vcf(vcfData, file = glue("{beagle_inputs_path}/unphased_genotypes.vcf.gz"))
-
-# generateMaskedFiles <- function(runNum, numMasked) {
-#   # split genotypes by linkage group
-#   chrGroupedAlleleFreqsData <- alleleFreqsData %>% group_by(Chr)
-#   dir.create(glue("{beagle_inputs_path}/masking/{runNum}_inputs"), showWarnings = TRUE)
-#   for (i in group_split(chrGroupedAlleleFreqsData)) {
-#     
-#     # write i to a separate file
-#     currLinkageGroup = as.character(i[1, "Chr"])
-#     dir.create(glue("{beagle_inputs_path}/masking/{runNum}_inputs/{currLinkageGroup}_inputs/"), showWarnings = TRUE)
-#   }
-#   
-#   # create separate vcf files per "Group"
-#   chrGroupedJoinedVcfData <- joined_vcf_table %>% group_by(Group)
-#   for (i in group_split(chrGroupedJoinedVcfData)) {
-#     currLinkageGroup = as.character(i[1, "Group"])
-#     
-#     # regenerate the vcf file format
-#     chrom_linkage_groups <- i$Group
-#     pos_vals <- i$RR_Pos
-#     id_vals <- rep(NA, length(pos_vals))
-#     ref_vals <- rep("A", length(pos_vals))
-#     alt_vals <- rep("G", length(pos_vals))
-#     qual_vals <- rep(99, length(pos_vals))
-#     filter_vals <- rep(NA, length(pos_vals))
-#     info_vals <- rep(NA, length(pos_vals))
-#     
-#     fixedData <- data.table(
-#       CHROM = chrom_linkage_groups,
-#       POS = pos_vals,
-#       ID = id_vals,
-#       REF = ref_vals,
-#       ALT = alt_vals,
-#       QUAL = qual_vals,
-#       FILTER = filter_vals,
-#       INFO = info_vals
-#     )
-#     
-#     cleaned_mm_colnames <- colnames(joined_vcf_table)
-#     filtered_genotype_data <- i[, cleaned_mm_colnames[cleaned_mm_colnames %in% name_mapper$safe_names]]
-#     
-#     filtered_genotype_data$FORMAT <- rep("GT", length(pos_vals))
-#     filtered_genotype_data <- filtered_genotype_data[, c(length(colnames(filtered_genotype_data)), 1:length(colnames(filtered_genotype_data)) - 1)]
-#     
-#     # masking
-#     hetLocs <- which(filtered_genotype_data == "0/1", arr.ind = TRUE)
-#     # randomly sample n hetLocs
-#     hetLocsSelected <- hetLocs[sample(nrow(hetLocs), numMasked), ]
-#     filtered_genotype_data_copy <- cbind(filtered_genotype_data)
-#     
-#     for (h in seq(1, nrow(hetLocsSelected))) {
-#       currHetLoc <- hetLocsSelected[h, ]
-#       filtered_genotype_data_copy[currHetLoc["row"], currHetLoc["col"]] = NA
-#     }
-#     # write masked hetLocs to file
-#     write.csv(hetLocsSelected, glue("{beagle_inputs_path}/masking/{runNum}_inputs/{currLinkageGroup}_inputs/masked_locations.csv"))
-#     
-#     genotypeData <- as.matrix(filtered_genotype_data_copy)
-#     
-#     # chromosome | rs# / snp identifier | genetic distance (morgans) | base-pair position
-#     # Chromosome | Group | cM / 100 | RR_Pos
-#     plink_map <- data.frame(
-#       Chromosome = i$Group,
-#       Group = i$Group, 
-#       Morgans = i$cM / 100, 
-#       Pos = i$RR_Pos
-#     )
-#     
-#     # sort to put map positions in ascending order
-#     plink_map <- plink_map[order(plink_map$Morgans), ]
-#     
-#     # modify the pos vals to be consistent with morgans ordering
-#     new_pos_vals <- plink_map$Pos
-#     old_pos_vals <- pos_vals
-#     
-#     for (j in seq(2, length(new_pos_vals))) {
-#       curr_val <- new_pos_vals[j]
-#       prev_val <- new_pos_vals[j - 1]
-#       if (prev_val >= curr_val) {
-#         new_pos_vals[j] <- prev_val + 1
-#       }
-#     }
-#     new_pos_matcher <- data.frame(old_pos_vals, new_pos_vals)
-#     
-#     plink_map$Pos <- new_pos_vals
-#     
-#     write.table(plink_map, glue("{beagle_inputs_path}/masking/{runNum}_inputs/{currLinkageGroup}_inputs/strawberry.map"), row.names = FALSE, col.names = FALSE, quote = FALSE, sep = "\t")
-#     
-#     fixed_pos_vals <- fixedData$POS
-#     for (j in seq(1, length(new_pos_vals))) {
-#       curr_fp_val <- fixed_pos_vals[j]
-#       
-#       # find curr_fp_val in new_pos_matcher
-#       match <- new_pos_matcher[new_pos_matcher$old_pos_vals == curr_fp_val, ][1, "new_pos_vals"]
-#       fixed_pos_vals[j] <- match
-#     }
-#     fixedData$POS <- fixed_pos_vals
-#     
-#     fixedData <- as.matrix(fixedData, row.names = NULL)
-#     vcfData <- new("vcfR", meta = metaData, fix = fixedData, gt = genotypeData)
-#     write.vcf(vcfData, file = glue("{beagle_inputs_path}/masking/{runNum}_inputs/{currLinkageGroup}_inputs/genotype_{currLinkageGroup}.vcf.gz"))
-#   }
-# }
-# 
-# numMaskedArgs <- c(2, 3, 5, 10, 15, 20, 50, 100, 200) # c(100, 200, 500, 1000, 2000)
-# for (i in seq(1, length(numMaskedArgs))) {
-#   generateMaskedFiles(i, numMaskedArgs[i])
-# }
+numMaskedArgs <- c(0, 2, 3, 5, 10, 15, 20, 50, 100, 200) # c(100, 200, 500, 1000, 2000)
+for (i in seq(1, length(numMaskedArgs))) {
+   generateMaskedFiles(i, numMaskedArgs[i])
+}
 
 ##### CHECK BEAGLE OUTPUTS
 
@@ -234,68 +148,47 @@ ratio <- c()
 
 # combine the output vcf files
 for (run_i in seq(1, length(numMaskedArgs))) {
-  full_fixed_data <- data.table(
-    CHROM = c(),
-    POS = c(),
-    ID = c(),
-    REF = c(),
-    ALT = c(),
-    QUAL = c(),
-    FILTER = c(),
-    INFO = c()
-  )
-  
-  full_masked <- data.table(
-    row = c(),
-    col = c()
-  )
-  
   het_phased_count <- 0
   total_het_count <- 0
   
   input_dirs <- list.dirs(path = glue("{beagle_inputs_path}/masking/{run_i}_inputs/"), full.names = TRUE)
   print(length(input_dirs))
   
-  for (i in seq(1, num_linkage_groups)) {
-    output_path <- glue("{beagle_outputs_path}/masking/{run_i}_outputs/{i}_phased_genotypes.vcf")
-    input_path <- glue("{input_dirs[i + 1]}/cleaned_genotypes.vcf")
-    het_locs_path <- glue("{input_dirs[i + 1]}/masked_locations.csv")
+  output_path <- glue("{beagle_outputs_path}/masking/{run_i}_outputs/1_phased_genotypes.vcf")
+  input_path <- glue("{beagle_inputs_path}/masking/{run_i}_inputs/cleaned_genotypes.vcf")
+  het_locs_path <- glue("{beagle_inputs_path}/masking/{run_i}_inputs/masked_locations.csv")
+  
+  if (numMaskedArgs[run_i] == 0) {
+    ratio <- c(ratio, 1)
+    next
+  }
+  
+  curr_output <- read.vcfR(output_path)
+  curr_input <- read.vcfR(input_path)
+  
+  curr_fixed_data <- as.data.table(curr_output@fix)
+  curr_unphased_genotype_data <- as.data.table(curr_input@gt)
+  curr_genotype_data <- as.data.table(curr_output@gt)
+  curr_het_locs_data <- as.data.frame(read.csv(het_locs_path))
+  
+  for (j in nrow(curr_het_locs_data)) {
+    idx <- curr_het_locs_data[j, ]
+    curr_out <- as.vector(unname(curr_genotype_data[idx$row]))
+    curr_in <- as.vector(unname(curr_unphased_genotype_data[idx$row]))
     
-    curr_output <- read.vcfR(output_path)
-    curr_input <- read.vcfR(input_path)
+    curr_in_unphased <- curr_out[[idx$col]]
+    curr_out_phased <- curr_out[[idx$col]]
     
-    curr_fixed_data <- as.data.table(curr_output@fix)
-    curr_unphased_genotype_data <- as.data.table(curr_input@gt)
-    curr_genotype_data <- as.data.table(curr_output@gt)
-    curr_het_locs_data <- as.data.frame(read.csv(het_locs_path))
-    
-    for (j in nrow(curr_het_locs_data)) {
-      idx <- curr_het_locs_data[j, ]
-      curr_out <- as.vector(unname(curr_genotype_data[idx$row]))
-      curr_in <- as.vector(unname(curr_unphased_genotype_data[idx$row]))
-      
-      curr_in_unphased <- curr_out[[idx$col]]
-      curr_out_phased <- curr_out[[idx$col]]
-      
-      print(curr_out_phased)
-      print(curr_in_unphased)
-      if (!is.na(curr_out_phased)) {
-        if (curr_out_phased == "0|1" || curr_out_phased == "1|0") {
-          het_phased_count = het_phased_count + 1;
-        }
+    print(curr_out_phased)
+    print(curr_in_unphased)
+    if (!is.na(curr_out_phased)) {
+      if (curr_out_phased == "0|1" || curr_out_phased == "1|0") {
+        het_phased_count = het_phased_count + 1;
       }
     }
-    
-    total_het_count = total_het_count + nrow(curr_het_locs_data)
-    
-    full_fixed_data <- rbindlist(list(full_fixed_data, curr_fixed_data))
-    full_masked <- rbindlist(list(full_masked, curr_het_locs_data))
-    if (i == 1) {
-      full_genotype_data <- curr_genotype_data
-    } else {
-      full_genotype_data <- rbindlist(list(full_genotype_data, curr_genotype_data))
-    }
   }
+  
+  total_het_count = total_het_count + nrow(curr_het_locs_data)
   
   ratio <- c(ratio, het_phased_count / total_het_count)
   print(glue("correctly phased heterozygous ratio {het_phased_count / total_het_count}"))
@@ -307,5 +200,5 @@ print(ratio)
 heterozygous_phased_ratio <- data.frame(num_masked = numMaskedArgs * num_linkage_groups, correctly_phased_ratio = ratio)
 head(heterozygous_phased_ratio)
 ggplot(data = heterozygous_phased_ratio, aes(x = num_masked, y = correctly_phased_ratio)) + geom_line() + geom_point() + ggtitle("Correctly Phased Heterozygous Ratio vs. Number of Masked Unphased Genotypes") + xlab("Number of Masked Unphased Genotypes") + ylab("Correctly Phased Heterozygous Ratio")
-
+ggsave(glue("{beagle_outputs_path}/masking/correctly_masked_ratio.png"))
 
